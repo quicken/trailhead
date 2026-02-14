77537b8a09dfb4cda7231d882cb7f316050b47de
marcel <mscherzer@gmail.com>
2026-02-14 22:55:31 +1000

    update shoelace example such that it can run in local development mode.

--- examples/shoelace-site/shell/src/index.ts
+++ examples/shoelace-site/shell/src/index.ts
@@ -6,12 +6,14 @@ import { ShoelaceAdapter, ShellApp } from '@herdingbits/trailhead-shoelace';
 
 // Get configuration
 const basePath = import.meta.env.VITE_BASE_PATH || "";
+const shellResourceUrl = (window as any).SHELL_DEV_URL || basePath;
 const apiUrl = (window as any).APP_CONFIG?.apiUrl || "";
 
 // Initialize shell with Shoelace adapter
 const shell = new Trailhead({
   adapter: new ShoelaceAdapter(),
   basePath,
+  shellResourceUrl,
   apiUrl,
 });
 
